e209e9ee1b772336f6349f33ed0090aef74fed29
Salajan Paul-Sebastian <salajanpaul1@gmail.com>
2024-06-20 16:16:18 +0300

    da

--- site.js
+++ site.js
@@ -1,6 +1,6 @@
 var abcd = document.getElementById("kw-250");
 abcd.addEventListener("click",whatever);
-// cum fac sa adaug mai multe elemente kw-250, kw-300 etc
+
 function whatever() {
     swal("Se incarca pagina..");
 }
